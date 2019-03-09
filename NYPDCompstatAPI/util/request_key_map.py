de7b54f7f9987f6bc64863f3bd30e3d773966050
Rukmal Weerawarana <rukmal.weerawarana@gmail.com>
2019-03-09 18:00:00 -0500

    Added mapping of crime types, apprehension location types, and crime groups

---
+++ NYPDCompstatAPI/util/request_key_map.py
@@ -0,0 +1,26 @@
+# Mapping of specific crimes
+CRIME = {
+    'murder': 'Murder',
+    'rape': 'Rape',
+    'robbery': 'Robbery',
+    'felony_assault': 'Fel. Assault',
+    'burglary': 'Burglary',
+    'grand_larceny': 'Gr. Larceny',
+}
+
+# Mapping of apprehension locations
+APPREHENSION_LOCATION = {
+    'patrol': 'PSB',
+    'transit': 'Transit',
+    'housing': 'Housing'
+}
+
+# Mapping of arbitrary crime "groups"
+CRIME_ARBITRARY_GROUP = {
+    'shooting_victims': 'Sht. Vic.',
+    'shooting_incidents': 'Sht. Inc.',
+    'rape_1': 'Rape 1',
+    'petit_larceny': 'Petit Larceny',
+    'misdemeanor_assault': 'Misd. Assault',
+    'misdemeanor_sex_crimes': 'Misd. Sex Crimes'
+}
